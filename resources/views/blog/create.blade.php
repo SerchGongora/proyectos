2035e9cb0eea41e0d7caf5e57d65f5c1be9eb98a
SerchGongora <sergio_gongora@openmailbox.org>
2016-12-02 15:52:57 -0600

    initial commit

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+  {{ trans('adminlte_lang::message.home') }}
+@endsection
+@section('main-content')
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Create Data</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <form class="" action="{{route('blog.store')}}" method="post">
+        {{csrf_field()}}
+        <div class="form-group{{ ($errors->has('title')) ? $errors->first('title') : '' }}">
+          <input type="text" name="title" class="form-control" placeholder="Enter Title Here">
+          {!! $errors->first('title','<p class="help-block">:message</p>') !!}
+        </div>
+        <div class="form-group{{ ($errors->has('description')) ? $errors->first('title') : '' }}">
+          <input type="text" name="description" class="form-control" placeholder="Enter Description Here">
+          {!! $errors->first('description','<p class="help-block">:message</p>') !!}
+        </div>
+        <div class="form-group">
+          <input type="submit" class="btn btn-primary" value="save">
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  @stop
---
+++ resources/views/blog/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+  {{ trans('adminlte_lang::message.home') }}
+@endsection
+@section('main-content')
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Edit Data</h1>
+    </div>
+  </div>
+  <div class="row">
+    <form class="" action="{{route('blog.update',$blog->id)}}" method="post">
+      <input name="_method" type="hidden" value="PATCH">
+      {{csrf_field()}}
+      <div class="form-group{{ ($errors->has('title')) ? $errors->first('title') : '' }}">
+        <input type="text" name="title" class="form-control" placeholder="Enter Title Here" value="{{$blog->title}}">
+        {!! $errors->first('title','<p class="help-block">:message</p>') !!}
+      </div>
+      <div class="form-group{{ ($errors->has('description')) ? $errors->first('title') : '' }}">
+        <input type="text" name="description" class="form-control" placeholder="Enter Description Here" value="{{$blog->description}}">
+        {!! $errors->first('description','<p class="help-block">:message</p>') !!}
+      </div>
+      <div class="form-group">
+        <input type="submit" class="btn btn-primary" value="save">
+      </div>
+    </form>
+  </div>
+  @stop
